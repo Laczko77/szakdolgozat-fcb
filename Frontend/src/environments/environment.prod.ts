045b7581e2002defadccc73165a40775e40f6769
Laczkovich Kristóf <h160880@stud.u-szeged.hu>
2025-05-23 10:03:22 +0200

    / jav 100x

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://szakdolgozat-fcb.onrender.com/api/',
+    apiUrl: 'https://szakdolgozat-fcb.onrender.com/api',
     emailjsServiceId: 'Szakdolgozat',
     emailjsTemplateId: 'template_vmilpps',
     emailjsTemplateIdCompleted: 'template_k5vt829',
